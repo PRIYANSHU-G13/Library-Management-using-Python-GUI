665e057969bd53ddfa58149aefd7555e4f9612b5
Kathyayani-Bolgam <70484748+Kathyayani-Bolgam@users.noreply.github.com>
2020-09-27 12:31:01 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -29,13 +29,13 @@ Canvas1.pack(expand=True,fill=BOTH)
 
 
 header_admin= Label(admin,text='ADMIN',fg='black',bg='#7d7d7d')
-header_admin.place(relx=0.65,rely=0.02,relwidth=0.1,relheight=0.05)
+header_admin.place(relx=0.65,rely=0.09,relwidth=0.1,relheight=0.05)
 
 header_username=Button(admin,text='USERNSME',fg='black')
-header_username.place(relx=0.76,rely=0.02,relwidth=0.2,relheight=0.05)
+header_username.place(relx=0.76,rely=0.09,relwidth=0.2,relheight=0.05)
 
 headingFrame1 = Frame(admin,bg="#FFBB00",bd=2)
-headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.08)
+headingFrame1.place(relx=0,rely=0.0,relwidth=1,relheight=0.08)
 
 headingLabel = Label(headingFrame1, text="Welcome ADMIN", bg='black', fg='white', font=('Courier',15))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
@@ -43,20 +43,55 @@ headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 # search entry
 search_entry=Entry(admin)
 search_entry.insert(0,'Search Students/Books')
-search_entry.place(relx=0.11,rely=0.3, relwidth=0.45, relheight=0.08)
+search_entry.place(relx=0.04,rely=0.15, relwidth=0.57, relheight=0.06)
 
 # Search Students
 sr1 = Button(admin,text="Search Students",bg='#7d7d7d',fg='black')#,command=SearchStudents)
-sr1.place(relx=0.57,rely=0.3, relwidth=0.16, relheight=0.08)
+sr1.place(relx=0.63,rely=0.15, relwidth=0.155, relheight=0.06)
 
 # Search books
 sr2 = Button(admin,text="Search Books",bg='#7d7d7d',fg='black')#,command=SearchBooks)
-sr2.place(relx=0.74,rely=0.3, relwidth=0.16, relheight=0.08)
+sr2.place(relx=0.80,rely=0.15, relwidth=0.155, relheight=0.06)
+
+# Manage Students
+
+lb1 = Menu(menubar,tearoff=0,bg='black', fg='white')
+menubar.add_cascade(label='Manage Students',menu=lb1)
+lb1.add_command(label='Add Student',command=None)
+lb1.add_command(label='View Students',command=None)
+
+
+# Manage Books
+
+lb2 = Menu(menubar,tearoff=0,bg='black', fg='white')
+menubar.add_cascade(label='Manage Books',menu=lb2)
+lb2.add_command(label='View Library',command=None)
+lb2.add_command(label='Issued Books',command=None)
+lb2.add_separator()
+lb2.add_command(label='Add Existing Book',command=None)
+lb2.add_command(label='Add New Book',command=None)
+lb2.add_command(label='Add Book Details',command=None)
+lb2.add_command(label='Delete Book',command=None)
+
+
+# Pending files
+lb3 = Menu(menubar,tearoff=0,bg='black', fg='white')
+menubar.add_cascade(label='Pending Files',menu=lb3)
+lb3.add_command(label='Pending Requests',command=None)
+lb3.add_command(label='Pending Books',command=None)
+
+
+# Query/Feedback
+
+lb4 = Menu(menubar,tearoff=0,bg='black', fg='white')
+menubar.add_cascade(label='Qwery/Feedback',menu=lb4)
+lb4.add_command(label='Feedback',command=None)
+lb4.add_command(label='Query',command=None)
 
 labelFrame = Frame(admin,bg='black')
-labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
+labelFrame.place(relx=0.04,rely=0.25,relwidth=0.92,relheight=0.6)
 
 quitBtn = Button(admin,text="Quit",bg='#f7f1e3', fg='black', command=admin.destroy)
-quitBtn.place(relx=0.12,rely=0.88, relwidth=0.18,relheight=0.08)
+quitBtn.place(relx=0.12,rely=0.9, relwidth=0.18,relheight=0.06)
 
 admin.mainloop()
