ad4405e1e8f7b0503802529a55577544b4610c8a
Priyanshu Gupta <66169836+PRIYANSHU-G13@users.noreply.github.com>
2020-10-11 09:43:32 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,31 +7,31 @@ import mysql.connector
 con = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="XXXX",#Replace XXXX with your MySQL password
+    password="root@0035",#Replace XXXX with your MySQL password
     database="Library")
-'''Create this database(Library) and add the student table as:
-create table Students(Fullname varchar(30) NOT NULL,Username varchar(15) NOT NULL,
-Email varchar(50) NOT NULL,Password varchar(20) NOT NULL,Contact int(10) NOT NULL,
-Age int(3) NOT NULL,Batch int(4) NOT NULL,Course varchar(20) NOT NULL,PRIMARY KEY (Username));'''
+        #'''Create this database(Library) and add the student table as:
+         #  create table Students(Fullname varchar(30) NOT NULL,Username varchar(15) NOT NULL,
+          # Email varchar(50) NOT NULL,Password varchar(20) NOT NULL,Contact int(10) NOT NULL,
+           #Age int(3) NOT NULL,Batch int(4) NOT NULL,Course varchar(20) NOT NULL,PRIMARY KEY (Username));'''
 
-'''books table as:
-create table books(B_ID VARCHAR(10) NOT NULL, B_NAME VARCHAR(20) NOT NULL, AUTHOR VARCHAR(30) NOT NULL,
-S_ID VARCHAR(10) NOT NULL, S_NAME VARCHAR(20) NOT NULL, C_NAME VARCHAR(20) NOT NULL,
-PRIMARY KEY (B_ID));'''
+        #'''add books table as:
+         #  create table books(B_ID VARCHAR(10) NOT NULL, B_NAME VARCHAR(20) NOT NULL, AUTHOR VARCHAR(30) NOT NULL,
+          # S_ID VARCHAR(10) NOT NULL, S_NAME VARCHAR(20) NOT NULL, C_NAME VARCHAR(20) NOT NULL,
+           #PRIMARY KEY (B_ID));'''
 
 cur = con.cursor()
 
 admin = Tk()
 admin.title("Admin")
 admin.minsize(width=400,height=400)
-admin.geometry("1000x800")
+admin.geometry("800x600")
 #creating menubar
 menubar = Menu(admin)
 
 
 
 Canvas1 = Canvas(admin)
-Canvas1.config(bg="pink")
+Canvas1.config(bg="#B8DBE6")
 Canvas1.pack(expand=True,fill=BOTH)
 
 '''def SearchStudents():
@@ -49,17 +49,17 @@ def adds():
         usern=str(username1.get())
         mail=str(email1.get())
         passwrd=str(password1.get())
-        con=str(contact1.get())
+        cont=str(contact1.get())
         age=str(age1.get())
         batch=str(batch1.get())
         course=str(course1.get())
-        if(fname=='' or usern=="" or mail=="" or con==None or age==None or batch=="" or course=="" or passwrd==""):
+        if(fname=='' or usern=="" or mail=="" or cont==None or age==None or batch=="" or course=="" or passwrd==""):
             messagebox.showinfo("Can't ADD","All feilds are required!")
         if(mail.endswith('@iiitkottayam.ac.in')):
             pass
         else:
             messagebox.showinfo("Invalid Email","Enter a valid Email ID!\nEmail ID should be of domain name '@iiitkottayam.ac.in'")
-        if(con.isnumeric() and len(con)==10):
+        if(cont.isnumeric() and len(cont)==10):
             pass
         else:
             messagebox.showinfo("Invalid Contact Number","Enter a valid Contact Number(Length 10)!")
@@ -76,15 +76,29 @@ def adds():
         else:
             messagebox.showinfo("Invalid Username","Username should be Alphanumeric!")
 
-        addformula="INSERT INTO students (Fullname, Username, Email, Password, Contact, Age, Batch, Course) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        add_student=(fname,usern,mail,passwrd,int(con),int(age),int(batch),course)
-        cur.execute(addformula, add_student)
-        con.commit()
-        messagebox.showinfo("Student Added","Student is added successfully!")
+        try:
+            addformula="INSERT INTO students (Fullname, Username, Email, Password, Contact, Age, Batch, Course) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            add_student=(fname,usern,mail,passwrd,int(cont),int(age),int(batch),course)
+            cur.execute(addformula, add_student)
+            con.commit()
+            fullname1.delete(0,'end')
+            username1.delete(0,'end')
+            email1.delete(0,'end')
+            password1.delete(0,'end')
+            contact1.delete(0,'end')
+            age1.delete(0,'end')
+            batch1.delete(0,'end')
+            course1.delete(0,'end')
+            messagebox.showinfo("Student Added","Student is added successfully!")
+
+
+        except(mysql.connector.errors.IntegrityError):
+            messagebox.showinfo("Can't add Student","Oops!!\nUsername already exist!!")
 
 
 
-    details=Label(labelFrame,text="Add Details:",bg='white',fg='black',font="bold").place(relx=0.08,rely=0.06,relwidth=0.3,relheight=0.1)
+
+    details=Label(labelFrame,text="Add Details:",bg='white',fg='black',font='BOLD').place(relx=0.08,rely=0.06,relwidth=0.3,relheight=0.1)
     fullname=Label(labelFrame,text="Full name",bg='black',fg='white').place(relx=0.1,rely=0.18,relwidth=0.25,relheight=0.08)
     username=Label(labelFrame,text="Username",bg='black',fg='white').place(relx=0.1,rely=0.28,relwidth=0.25,relheight=0.08)
     email=Label(labelFrame,text="Email Id",bg='black',fg='white').place(relx=0.1,rely=0.38,relwidth=0.25,relheight=0.08)
@@ -120,8 +134,39 @@ def views():
     for widget in labelFrame.winfo_children():
         widget.destroy()
 
-    view=Button(labelFrame,text='View',bg='#7d7d7d',fg='black').place(relx=0.7,rely=0.06,relwidth=0.25,relheight=0.1)
+    lboxs = Listbox(labelFrame, bg="black", relief="sunken",font=("courier",15),fg="white")
+    lboxs.place(relx=0.04, rely=0.3,relwidth=0.92,relheight=0.65)
 
+    Name = Label(labelFrame,text="NAME",bg="white",fg="black")
+    Name.place(relx=0.04,rely=0.2,relwidth=0.08,relheight=0.06)
+
+    User = Label(labelFrame,text="USERNAME",bg="white",fg="black")
+    User.place(relx=0.16,rely=0.2,relwidth=0.1,relheight=0.06)
+
+    E_Mail= Label(labelFrame,text="E-MAIL",bg="white",fg="black")
+    E_Mail.place(relx=0.35,rely=0.2,relwidth=0.1,relheight=0.06)
+
+    Batch = Label(labelFrame,text="BATCH",bg="white",fg="black")
+    Batch.place(relx=0.518,rely=0.2,relwidth=0.1,relheight=0.06)
+
+    Course = Label(labelFrame,text="COURSE",bg="white",fg="black")
+    Course.place(relx=0.68,rely=0.2,relwidth=0.1,relheight=0.06)
+
+    Message = Label(labelFrame,text="MESSAGE",bg="white",fg="black")
+    Message.place(relx=0.82,rely=0.2,relwidth=0.14,relheight=0.06)
+
+    def show():
+        lboxs.delete(0,'end')
+
+        cur.execute("select * from students")
+        rows=cur.fetchall()
+
+        for row in rows:
+            insertdata = "%-12.5s%-10s%-20s%-10s%-10s%-12.5s"%(row[0],row[1],row[2],row[6],row[7],'Message')
+            lboxs.insert(lboxs.size()+1, insertdata)
+
+    display = Button(labelFrame,text="CLICK TO VIEW",bg='#525b59', fg='white',command=show)
+    display.place(relx=0.35,rely=0.05,relwidth=0.3,relheight=0.1)
 
 # Deleting Student from the Database
 def deletes():
@@ -137,22 +182,24 @@ def deletes():
             messagebox.showinfo("Can't Delete","All feilds are required!")
         cur.execute("DELETE FROM Students WHERE Username='"+ delst +"'")
         con.commit()
+        dusername.delete(0,'end')
+        dfullname.delete(0,'end')
+        demail.delete(0,'end')
         messagebox.showinfo("Student Deleted","Student is Deleted successfully!")
 
-
-    details=Label(labelFrame,text="Enter Details:",bg='white',fg='black',font="bold").place(relx=0.08,rely=0.06,relwidth=0.3,relheight=0.1)
+    details=Label(labelFrame,text="Enter Details:",bg='white',fg='black',font='BOLD').place(relx=0.08,rely=0.06,relwidth=0.3,relheight=0.1)
     username=Label(labelFrame,text="Username",bg='black',fg='white').place(relx=0.1,rely=0.28,relwidth=0.25,relheight=0.08)
     fullname=Label(labelFrame,text="Full name",bg='black',fg='white').place(relx=0.1,rely=0.38,relwidth=0.25,relheight=0.08)
     email=Label(labelFrame,text="Email Id",bg='black',fg='white').place(relx=0.1,rely=0.48,relwidth=0.25,relheight=0.08)
-    dfullname = Entry(labelFrame)
-    dfullname.place(relx=0.5,rely=0.38,relwidth=0.3,relheight=0.08)
     dusername = Entry(labelFrame)
     dusername.place(relx=0.5,rely=0.28,relwidth=0.3,relheight=0.08)
+    dfullname = Entry(labelFrame)
+    dfullname.place(relx=0.5,rely=0.38,relwidth=0.3,relheight=0.08)
     demail = Entry(labelFrame)
     demail.place(relx=0.5,rely=0.48,relwidth=0.3,relheight=0.08)
     delete=Button(labelFrame,text='Delete',bg='#7d7d7d',fg='black',command=del_stu).place(relx=0.7,rely=0.06,relwidth=0.25,relheight=0.1)
 
-header_admin= Label(admin,text='ADMIN',fg='black',bg='#7d7d7d')
+header_admin= Label(admin,text='ADMIN',bg='#525b59', fg='white')
 header_admin.place(relx=0.65,rely=0.09,relwidth=0.1,relheight=0.05)
 
 header_username=Button(admin,text='USERNAME',fg='black')
@@ -170,26 +217,26 @@ search_entry.insert(0,'Search Students/Books')
 search_entry.place(relx=0.04,rely=0.15, relwidth=0.57, relheight=0.06)
 
 # Search Students
-sr1 = Button(admin,text="Search Students",bg='#7d7d7d',fg='black')#,command=SearchStudents)
+sr1 = Button(admin,text="Search Students",bg='#525b59', fg='white')#,command=SearchStudents)
 sr1.place(relx=0.63,rely=0.15, relwidth=0.155, relheight=0.06)
 
 # Search books
-sr2 = Button(admin,text="Search Books",bg='#7d7d7d',fg='black')#,command=SearchBooks)
+sr2 = Button(admin,text="Search Books",bg='#525b59', fg='white')#,command=SearchBooks)
 sr2.place(relx=0.80,rely=0.15, relwidth=0.155, relheight=0.06)
 
 labelFrame = Frame(admin,bg="black")
-labelFrame.place(relx=0.04,rely=0.25,relwidth=0.92,relheight=0.6)
+labelFrame.place(relx=0.04,rely=0.25,relwidth=0.92,relheight=0.64)
 
 
 # Manage Students
 
-lb1 = Menu(menubar,tearoff=0,bg='black', fg='white')
+lb1 = Menu(menubar,tearoff=0,bg='#525b59', fg='white')
 menubar.add_cascade(label='Manage Students',menu=lb1)
 lb1.add_command(label='Add Student',command=adds)
 lb1.add_command(label='View Students',command=views)
 lb1.add_separator()
 lb1.add_command(label='Delete Student',command=deletes)
-lb1.add_command(label='Restrict Student',command=None)
+#lb1.add_command(label='Restrict Student',command=None)
 
 
 
@@ -221,6 +268,8 @@ def view():
     c_name.place(relx=0.82,rely=0.2,relwidth=0.14,relheight=0.06)
 
     def show():
+        ls.delete(0,'end')
+
         cur.execute("select * from books")
         rows=cur.fetchall()
 
@@ -228,7 +277,7 @@ def view():
             insertdata = "%-10s%-15s%-15s%-10s%-12.5s%-12.5s"%(row[0],row[1],row[2],row[3],row[4],row[5])
             ls.insert(ls.size()+1, insertdata)
 
-    display = Button(labelFrame,text="CLICK TO VIEW",bg="pink",fg="black",command=show)
+    display = Button(labelFrame,text="CLICK TO VIEW",bg='#525b59', fg='white',command=show)
     display.place(relx=0.35,rely=0.05,relwidth=0.3,relheight=0.1)
 
 
@@ -242,45 +291,41 @@ def add_existing():
 def add_new():
     for widget in labelFrame.winfo_children():
         widget.destroy()
+    B_ID = Label(labelFrame,text="BOOK_ID",fg="black",bg="white")
+    B_ID.place(relx=0.02,rely=0.12,relwidth=0.3,relheight=0.08)
 
-    but1 = Button(labelFrame,text="ADD DETAILS",font="bold",fg="black",bg="white")
-    but1.place(relx=0.05,rely=0.12,relwidth=0.22,relheight=0.08)
+    B_NAME = Label(labelFrame,text="BOOK_NAME",fg="black",bg="white")
+    B_NAME.place(relx=0.02,rely=0.22,relwidth=0.3,relheight=0.08)
 
-    B_ID = Label(labelFrame,text="BOOK_ID",fg="white",bg="black")
-    B_ID.place(relx=0.02,rely=0.22,relwidth=0.3,relheight=0.08)
+    AUTHOR = Label(labelFrame,text="AUTHOR",fg="black",bg="white")
+    AUTHOR.place(relx=0.02,rely=0.32,relwidth=0.3,relheight=0.08)
 
-    B_NAME = Label(labelFrame,text="BOOK_NAME",fg="white",bg="black")
-    B_NAME.place(relx=0.02,rely=0.32,relwidth=0.3,relheight=0.08)
+    S_ID = Label(labelFrame,text="STREAM_ID",fg="black",bg="white")
+    S_ID.place(relx=0.02,rely=0.42,relwidth=0.3,relheight=0.08)
 
-    AUTHOR = Label(labelFrame,text="AUTHOR",fg="white",bg="black")
-    AUTHOR.place(relx=0.02,rely=0.42,relwidth=0.3,relheight=0.08)
+    S_NAME = Label(labelFrame,text="STREAM_NAME",fg="black",bg="white")
+    S_NAME.place(relx=0.02,rely=0.52,relwidth=0.3,relheight=0.08)
 
-    S_ID = Label(labelFrame,text="STREAM_ID",fg="white",bg="black")
-    S_ID.place(relx=0.02,rely=0.52,relwidth=0.3,relheight=0.08)
-
-    S_NAME = Label(labelFrame,text="STREAM_NAME",fg="white",bg="black")
-    S_NAME.place(relx=0.02,rely=0.62,relwidth=0.3,relheight=0.08)
-
-    C_NAME = Label(labelFrame,text="CATEGORY",fg="white",bg="black")
-    C_NAME.place(relx=0.02,rely=0.72,relwidth=0.3,relheight=0.08)
+    C_NAME = Label(labelFrame,text="CATEGORY",fg="black",bg="white")
+    C_NAME.place(relx=0.02,rely=0.62,relwidth=0.3,relheight=0.08)
 
     e_b_id = Entry(labelFrame)
-    e_b_id.place(relx=0.42,rely=0.22,relwidth=0.3,relheight=0.08)
+    e_b_id.place(relx=0.42,rely=0.12,relwidth=0.3,relheight=0.08)
 
     e_b_name = Entry(labelFrame)
-    e_b_name.place(relx=0.42,rely=0.32,relwidth=0.3,relheight=0.08)
+    e_b_name.place(relx=0.42,rely=0.22,relwidth=0.3,relheight=0.08)
 
     e_author = Entry(labelFrame)
-    e_author.place(relx=0.42,rely=0.42,relwidth=0.3,relheight=0.08)
+    e_author.place(relx=0.42,rely=0.32,relwidth=0.3,relheight=0.08)
 
     e_s_id = Entry(labelFrame)
-    e_s_id.place(relx=0.42,rely=0.52,relwidth=0.3,relheight=0.08)
+    e_s_id.place(relx=0.42,rely=0.42,relwidth=0.3,relheight=0.08)
 
     e_s_name = Entry(labelFrame)
-    e_s_name.place(relx=0.42,rely=0.62,relwidth=0.3,relheight=0.08)
+    e_s_name.place(relx=0.42,rely=0.52,relwidth=0.3,relheight=0.08)
 
     e_c_name = Entry(labelFrame)
-    e_c_name.place(relx=0.42,rely=0.72,relwidth=0.3,relheight=0.08)
+    e_c_name.place(relx=0.42,rely=0.62,relwidth=0.3,relheight=0.08)
 
 
     def add():
@@ -292,25 +337,19 @@ def add_new():
         C_NAME = e_c_name.get()
 
         if(B_ID=="" or B_NAME=="" or AUTHOR=="" or S_ID=="" or S_NAME=="" or C_NAME==""):
-            messagebox.showinfo("Invalid Detalis", "All fields are required!")
-        if(B_ID.isalnum()):
-            pass
+            messagebox.showinfo("Insert Status", "All fields are required!")
         else:
-            messagebox.showinfo("Invalid Details","BOOK_ID should be alphanumeric!")
-        if(S_ID.isalnum()):
-            pass
-        else:
-            messagebox.showinfo("Invalid Details","STREAM_ID should be alphanumeric!")
-        cur.execute("insert into books values('"+ B_ID +"','"+ B_NAME +"','"+ AUTHOR +"','"+ S_ID +"','"+ S_NAME +"','"+ C_NAME +"')")
-        cur.execute("commit")
-        messagebox.showinfo("Insert Status","Successfully inserted into database")
+            cur.execute("insert into books values('"+ B_ID +"','"+ B_NAME +"','"+ AUTHOR +"','"+ S_ID +"','"+ S_NAME +"','"+ C_NAME +"')")
+            cur.execute("commit")
 
-        e_b_id.delete(0, 'end')
-        e_b_name.delete(0, 'end')
-        e_author.delete(0, 'end')
-        e_s_id.delete(0, 'end')
-        e_s_name.delete(0, 'end')
-        e_c_name.delete(0, 'end')
+            e_b_id.delete(0, 'end')
+            e_b_name.delete(0, 'end')
+            e_author.delete(0, 'end')
+            e_s_id.delete(0, 'end')
+            e_s_name.delete(0, 'end')
+            e_c_name.delete(0, 'end')
+
+            messagebox.showinfo("Insert Status","Successfully inserted into database")
 
     ADD = Button(labelFrame,text="ADD BOOK",fg="black",bg="#7d7d7d",command=add)
     ADD.place(relx=0.62,rely=0.85,relwidth=0.2,relheight=0.08)
@@ -320,21 +359,17 @@ def add_new():
 def delete():
     for widget in labelFrame.winfo_children():
         widget.destroy()
+    B_ID = Label(labelFrame,text="BOOK_ID",fg="black",bg="white")
+    B_ID.place(relx=0.02,rely=0.12,relwidth=0.3,relheight=0.08)
 
-    but1 = Button(labelFrame,text="ADD DETAILS",font="bold",fg="black",bg="white")
-    but1.place(relx=0.14,rely=0.12,relwidth=0.22,relheight=0.08)
-
-    B_ID = Label(labelFrame,text="BOOK_ID",fg="white",bg="black")
-    B_ID.place(relx=0.1,rely=0.32,relwidth=0.3,relheight=0.08)
-
-    B_NAME = Label(labelFrame,text="BOOK_NAME",fg="white",bg="black")
-    B_NAME.place(relx=0.1,rely=0.42,relwidth=0.3,relheight=0.08)
+    B_NAME = Label(labelFrame,text="BOOK_NAME",fg="black",bg="white")
+    B_NAME.place(relx=0.02,rely=0.22,relwidth=0.3,relheight=0.08)
 
     e_b_id = Entry(labelFrame)
-    e_b_id.place(relx=0.5,rely=0.32,relwidth=0.3,relheight=0.08)
+    e_b_id.place(relx=0.42,rely=0.12,relwidth=0.3,relheight=0.08)
 
     e_b_name = Entry(labelFrame)
-    e_b_name.place(relx=0.5,rely=0.42,relwidth=0.3,relheight=0.08)
+    e_b_name.place(relx=0.42,rely=0.22,relwidth=0.3,relheight=0.08)
 
     def remove():
         B_ID = e_b_id.get()
@@ -353,11 +388,11 @@ def delete():
         messagebox.showinfo("Delete Status","Successfully deleted from database")
 
     DELETE = Button(labelFrame,text="DELETE BOOK",fg="black",bg="#7d7d7d",command=remove)
-    DELETE.place(relx=0.55,rely=0.65,relwidth=0.2,relheight=0.08)
+    DELETE.place(relx=0.62,rely=0.85,relwidth=0.2,relheight=0.08)
 
 # Manage Books
 
-lb2 = Menu(menubar,tearoff=0,bg='black', fg='white')
+lb2 = Menu(menubar,tearoff=0,bg='#525b59', fg='white')
 menubar.add_cascade(label='Manage Books',menu=lb2)
 lb2.add_command(label='View Library',command=view)
 lb2.add_command(label='Issued Books',command=None)
@@ -369,7 +404,7 @@ lb2.add_command(label='Delete Book',command=delete)
 
 
 # Pending files
-lb3 = Menu(menubar,tearoff=0,bg='black', fg='white')
+lb3 = Menu(menubar,tearoff=0,bg='#525b59', fg='white')
 menubar.add_cascade(label='Pending Files',menu=lb3)
 lb3.add_command(label='Pending Requests',command=None)
 lb3.add_command(label='Pending Books',command=None)
@@ -377,14 +412,14 @@ lb3.add_command(label='Pending Books',command=None)
 
 # Query/Feedback
 
-lb4 = Menu(menubar,tearoff=0,bg='black', fg='white')
+lb4 = Menu(menubar,tearoff=0,bg='#525b59', fg='white')
 menubar.add_cascade(label='Qwery/Feedback',menu=lb4)
 lb4.add_command(label='Feedback',command=None)
 lb4.add_command(label='Query',command=None)
 
 
 quitBtn = Button(admin,text="Quit",bg='#f7f1e3', fg='black', command=admin.destroy)
-quitBtn.place(relx=0.12,rely=0.9, relwidth=0.18,relheight=0.06)
+quitBtn.place(relx=0.05,rely=0.92, relwidth=0.18,relheight=0.05)
 
 admin.config(menu=menubar)
 admin.mainloop()
