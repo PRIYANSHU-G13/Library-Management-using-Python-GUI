93ecd4138f8bf4b1f80a3690eaa5372be0886401
Shivam Kr <71126664+Shivam01123@users.noreply.github.com>
2020-10-18 09:14:53 +0530

    Update c_login.py

--- c_login.py
+++ c_login.py
@@ -1,89 +1,187 @@
-from tkinter import *
-from PIL import ImageTk,Image
-import pymysql
+from io import BytesIO
 from tkinter import messagebox
-'''
-from AddBook import *
-from DeleteBook import *
-from ViewBooks import *
-from IssueBook import *
-from ReturnBook import *
-'''
-def Login():
-    global username, password 
-    user = str(username.get())
-    password1 = str(password.get())
-    command1 = "select * from admin where id=%s AND password=%s"
-    var=[user,password1]
-    command2 = "select * from student where id=%s AND password=%s"
+import tkinter
+from tkinter import font as tkFont
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import *
+from tkinter.ttk import *
+import tkinter.font as font
+from PIL import ImageTk, Image
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image
+import mysql.connector
+from admin import *
+from c_student import *
+
+
+
+win = Tk()
+win.title('Library Management System   |   Welcome to IIIT Kottayam Library')
+win.geometry('1150x680')
+win.config(bg='White')
+win.resizable(False, False)
+
+n = 1
+username = password = ""
+isempty=True
+
+#search_user funtion will be used to serch username and password into database and then it will open the next window 
+def search_user():
+    global n,username,password
+    found=False
+    command1 = "select Aid, Password from Admin where Aid=%s AND Password=%s"
+    var=[username,password]
+    command2 = "select Regno, Password from Student where Regno=%s AND Password=%s"
     try:
         # Add your own database name and password here to reflect in the code
         # before running this program create a databse python_pro
         #Create tables admin and student in database python_pro  
-        mypass = "your_MySQL_password"
-        mydatabase="python_pro"
-        con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
+        con = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="XXXX",#Replace XXXX with your MySQL password
+            database="Library")
         cur = con.cursor()
         cur.execute(command1,var)
-        var1=cur.fetchall()
+        admin=cur.fetchall()
         cur.execute(command2,var)
-        var2=cur.fetchall()
+        user=cur.fetchall()
         
-        if (len(var1)+len(var2))<1:
-          messagebox.showinfo('_','Invalid user name or password') 
-        else: 
-          for i in var1:
-            if i[0]==user and i[1]==password1:
-              messagebox.showinfo('sucess','Login Success as Admin')
-          for i in var2:
-            if i[0]==user and i[1]==password1:
-              messagebox.showinfo('sucess','Login Success as Student')               
+        if (len(admin)+len(user))<1:
+          messagebox.showinfo('Error!','Invalid user name or password') 
+          #back() will erase the data entered by user 
+          back()
+        else:          
+            if len(admin)>0:
+                messagebox.showinfo('sucess','Login Success as Admin')
+                cur.close()
+                con.close()
+                found=True
+                back()
+                start_admin(admin[0][0],admin[0][1])
+                #start_admin(admin[0][0],admin[0][1])  -----> admin function call
+                #admin function call will be there
+                #win.destroy()
+            if(found!=True):  
+                cur.close()
+                con.close()
+                back()
+                #messagebox.showinfo('sucess','Login Success as Student')
+                start_student(user[0][0],user[0][1])
+                #win.destroy()             
     except:
-        messagebox.showinfo("Error","Can't find data into Database")
-
-
-
-root = Tk()
-root.title("Library")
-root.minsize(width=400,height=400)
-root.geometry("600x500")
-
-
-    
-Canvas1 = Canvas(root)
-Canvas1.config(bg="pink")
-Canvas1.pack(expand=True,fill=BOTH)
-
-headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
-
-headingLabel = Label(headingFrame1, text="Welcome to \n Team Rolex Library", bg='black', fg='white', font=('Courier',15))
-headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
-
-labelFrame = Frame(root,bg='black')
-labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
-
-# User name
-lb1 = Label(labelFrame,text="User name : ", bg='black', fg='white')
-lb1.place(relx=0.05,rely=0.3, relheight=0.1)
+        found=False
+        back()
+        messagebox.showinfo("Error!","Something went wrong")
+        #back() will erase the data entered by user 
         
-username = Entry(labelFrame)
-username.place(relx=0.3,rely=0.3, relwidth=0.5, relheight=0.1)
 
-# Password
-lb2 = Label(labelFrame,text="Password : ", bg='black', fg='white')
-lb2.place(relx=0.05,rely=0.5, relheight=0.1)
-        
-password = Entry(labelFrame)
-password.place(relx=0.3,rely=0.5, relwidth=0.5, relheight=0.1)
-password.config(show="*")
 
-#Submit Button
-SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=Login)
-SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+
+
+
+def next_or_login():
+    global n,username,password,isempty
+    if(login_input.get() == '' or isempty):
+        #print("Empty input")
+        messagebox.showinfo("Error!", "Input field can't be empty")
+        return
     
-quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
-quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
+    #if n==1 then read username from login_input
+    if n == 1:
+        username = str(login_input.get())
+
+        login_input.delete(0, END)
+        backbt.place(relx=0.345555, rely=0.58+0.1, anchor=CENTER)
+        unsername_display.configure(text=username)
+        unsername_display.place(relx=0.5, rely=0.5, anchor=CENTER)
+        n = n+1
+        login.config(text="Login")
+
+        #start
+        isempty=True
+        login_input.insert(0,"Password")     
+        login_input.configure(state=DISABLED)                       
+        login_input.place(relx=0.5, rely=0.5+0.08, anchor=CENTER)
+        onclick_id=login_input.bind('<Button-1>', placeholder)
+        #end
+        return
+
+    #if n>=2 then read password from login_input
+    if n >= 2:
+        password = str(login_input.get())
+        # from here login process will start
+        search_user()
+
+def back():
+    global n
+    login_input.delete(0, END)
+    n = 1
+    login_input.config(show='')
+    unsername_display.place_forget()
+    backbt.place_forget()
+    login.config(text="Next")
+    #start
+    isempty=True
+    login_input.configure(state=NORMAL)
+    login_input.delete(0, END)
+    login_input.insert(0,"Username")     
+    login_input.configure(state=DISABLED)                       
+    login_input.place(relx=0.5, rely=0.5+0.08, anchor=CENTER)
+    onclick_id=login_input.bind('<Button-1>', placeholder)
+    #end
+
+def placeholder(event):
+    global isempty,n
+    isempty=False
+    login_input.configure(state=NORMAL)
+    login_input.delete(0, END)
+    try:
+        login_input.unbind('<Button-1>', onclick_id)  
+    except:
+        print()    
+    if n>1:
+        login_input.config(show="*")
+
+#Next button
+buttonsFont = tkFont.Font(family='product sans', size=15)
+login = tkinter.Button(win, text="Next", bg="#4285F4", activebackground="#356AC3", activeforeground="white",
+                       font=buttonsFont, anchor='c', width=13, relief="flat", fg="white",command=next_or_login)
+login.place(relx=0.655555, rely=0.58+0.1, anchor=CENTER)
+
+# Login Picture
+#img = PhotoImage(file="D:\\team_project\\project\\login_logo.png")
+img = PhotoImage(file="C:\\Users\\91969\\Desktop\\SEM 3\\IT WORKSHOP 3\\Project\\Project_code\\Student_section\\Student\\Changable_code\\login_logo.png")
+login_logo = tkinter.Label(image=img, bg="white", height=250, width=250)
+login_logo.place(relx=0.5, rely=0.25, anchor=CENTER)
+
+# Login input
+entryFont = tkFont.Font(family='product sans', size=15)
+login_input = tkinter.Entry(win, width=38, highlightthickness=3, font=entryFont, selectborderwidth=3,
+                            relief='flat', bg="pink", selectbackground="white")   
+#start                                                     
+login_input.insert(0,"Username")     
+login_input.configure(state=DISABLED)                       
+login_input.place(relx=0.5, rely=0.5+0.08, anchor=CENTER)
+onclick_id=login_input.bind('<Button-1>', placeholder)
+#end
+login_input.bind('<Return>', (lambda event: next_or_login()))
+login_input.bind('<Escape>', (lambda event: next_or_login()))
+
+# Login buttons
+buttonsFont = tkFont.Font(family='product sans', size=15)
+
+#Label to display user name after clicking on next button
+unsername_display = tkinter.Label(bg="white", borderwidth=1, font=buttonsFont)
+
+# Back Button
+backbt = tkinter.Button(win, text="Back", bg="#4285F4", activebackground="#356AC3", activeforeground="white",
+                        font=buttonsFont, anchor='c', width=13, relief="flat", fg="white", command=back)
 
 
-root.mainloop()
+win.mainloop()
